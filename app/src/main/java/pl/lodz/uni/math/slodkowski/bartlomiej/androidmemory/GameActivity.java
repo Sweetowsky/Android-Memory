d98d09b8481e510106d569f53a4048f293bc3352
Sweetowsky <barkosrm7@gmail.com>
2019-02-04 18:44:11 +0100

    Add buttons

---
+++ app/src/main/java/pl/lodz/uni/math/slodkowski/bartlomiej/androidmemory/GameActivity.java
@@ -0,0 +1,27 @@
+package pl.lodz.uni.math.slodkowski.bartlomiej.androidmemory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        GridView gridview = (GridView) findViewById(R.id.gridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(GameActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
